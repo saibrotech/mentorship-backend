aaa7152ee67db43bd2466eb0f10ea5f6b24401b9
Andre Piantino <andre@saibro.tech>
2022-08-08 10:26:28 -0300

    CU-2x0jpkt - Connected to postgres

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # mentorship-backend
+
+## Dev
+
+## Create environment
+
+`python3 -m venv venv`
+
+Select the interpreter before:
+
+`pip install -r requirements.txt`
+
+## Run server
+
+`python manage.py migrate`
+
+`python manage.py runserver`
--- mentorship/settings.py
+++ mentorship/settings.py
@@ -75,11 +75,14 @@ WSGI_APPLICATION = 'mentorship.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': 5432,
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
