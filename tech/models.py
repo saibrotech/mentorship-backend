ff1e4977660b416277953617ff03ac6f4b3af09e
Marianne Costa Avalone <marianne.avalone@gmail.com>
2022-09-01 14:42:45 -0300

    added companies model

--- tech/models.py
+++ tech/models.py
@@ -31,4 +31,23 @@ class Jobs(models.Model):
     type = models.CharField(max_length=6, choices=JOB_TYPES)
     location = models.CharField(max_length=50)
     requirements = models.TextField()
-    link = models.CharField(max_length=200)    
+    link = models.CharField(max_length=200)
+
+
+class Companies(models.Model):
+    COMPANY_SIZES = (                   # Source: SEBRAE. Anuario do Trabalho na Micro e Pequena Empresa. Sebrae: Brasília. p.17. 2013.
+        ('XS', 'Up to 10 employees'),   # https://www.sebrae.com.br/Sebrae/Portal%20Sebrae/Anexos/Anuario%20do%20Trabalho%20Na%20Micro%20e%20Pequena%20Empresa_2013.pdf
+        ('S', '10 to 49 employees'),
+        ('M', '50 to 99 employees'),
+        ('L', 'Over 100 employees')
+    )
+    
+    company_id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    overview = models.TextField()
+    website = models.CharField(max_length=200)
+    industry = models.CharField(max_length=50)
+    size = models.CharField(max_length=2, choices=COMPANY_SIZES)
+    headquarters = models.CharField(max_length=50)
+    founded = models.CharField(max_length=4)
+    specialties = models.TextField()
