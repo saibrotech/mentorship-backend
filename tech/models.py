7541e8abd9a33f01cd9fee240aae6492538573fa
Marianne Costa Avalone <marianne.avalone@gmail.com>
2022-09-02 15:29:09 -0300

    Added models

--- tech/models.py
+++ tech/models.py
@@ -1,5 +1,6 @@
 # https://docs.djangoproject.com/en/4.0/topics/db/models/
 
+from unicodedata import category
 from django.db import models
 
 # Create your models here.
@@ -9,7 +10,29 @@ class Language(models.Model):
     origin_country = models.CharField(max_length=20)
     origin_year = models.DateField()
 
-class Jobs(models.Model):
+class Category(models.Model):
+    main_stacks = models.CharField(max_length=200)
+    main_skills = models.CharField(max_length=200)
+    pay_range = models.CharField(max_length=10)
+
+class Company(models.Model):
+    COMPANY_SIZES = (                   # Source: SEBRAE. Anuario do Trabalho na Micro e Pequena Empresa. Sebrae: Brasília. p.17. 2013.
+        ('XS', 'Up to 10 employees'),   # https://www.sebrae.com.br/Sebrae/Portal%20Sebrae/Anexos/Anuario%20do%20Trabalho%20Na%20Micro%20e%20Pequena%20Empresa_2013.pdf
+        ('S', '10 to 49 employees'),
+        ('M', '50 to 99 employees'),
+        ('L', 'Over 100 employees')
+    )
+    
+    name = models.CharField(max_length=50)
+    overview = models.TextField()
+    website = models.CharField(max_length=200)
+    industry = models.CharField(max_length=50)
+    size = models.CharField(max_length=2, choices=COMPANY_SIZES)
+    headquarters = models.CharField(max_length=50)
+    founded = models.CharField(max_length=4)
+    specialties = models.TextField()
+
+class Job(models.Model):
     XP_LEVELS = (
         ('INTERN', 'Internship'),
         ('ENTRY', 'Entry Level'),
@@ -24,7 +47,6 @@ class Jobs(models.Model):
         ('OTHER', 'Other')
     )
 
-    listing_id = models.BigAutoField(primary_key=True)
     title = models.CharField(max_length=50)
     date_posted = models.DateField
     experience_level = models.CharField(max_length=6, choices=XP_LEVELS)
@@ -32,22 +54,6 @@ class Jobs(models.Model):
     location = models.CharField(max_length=50)
     requirements = models.TextField()
     link = models.CharField(max_length=200)
+    category_id = models.ForeignKey(Category, on_delete=models.CASCADE)
+    company_id = models.ForeignKey(Company, on_delete=models.CASCADE)
 
-
-class Companies(models.Model):
-    COMPANY_SIZES = (                   # Source: SEBRAE. Anuario do Trabalho na Micro e Pequena Empresa. Sebrae: Brasília. p.17. 2013.
-        ('XS', 'Up to 10 employees'),   # https://www.sebrae.com.br/Sebrae/Portal%20Sebrae/Anexos/Anuario%20do%20Trabalho%20Na%20Micro%20e%20Pequena%20Empresa_2013.pdf
-        ('S', '10 to 49 employees'),
-        ('M', '50 to 99 employees'),
-        ('L', 'Over 100 employees')
-    )
-    
-    company_id = models.BigAutoField(primary_key=True)
-    name = models.CharField(max_length=50)
-    overview = models.TextField()
-    website = models.CharField(max_length=200)
-    industry = models.CharField(max_length=50)
-    size = models.CharField(max_length=2, choices=COMPANY_SIZES)
-    headquarters = models.CharField(max_length=50)
-    founded = models.CharField(max_length=4)
-    specialties = models.TextField()
