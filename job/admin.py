55b2dc0c58e943272c84f0dbf069f107d64090c0
Marianne Costa Avalone <marianne.avalone@gmail.com>
2022-09-06 14:17:54 -0300

    Added search field

--- job/admin.py
+++ job/admin.py
@@ -8,3 +8,15 @@ from .models import Job
 admin.site.register(Category)
 admin.site.register(Company)
 admin.site.register(Job)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id','main_stacks', 'main_skills', 'pay_range')
+    search_fields = ['id','main_stacks', 'main_skills']
+
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ('id','name', 'overview', 'website', 'industry', 'size', 'headquarters', 'founded', 'specialties')
+    search_fields = ['id', 'name', 'industry', 'headquarters', 'founded', 'specialties']
+
+class JobAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'date_posted', 'type', 'location', 'requirements', 'link', 'category_id', 'company_id')
+    search_fields = ['id', 'title', 'date_posted', 'type', 'locatiion', 'requirements', 'category_id', 'company_id']
