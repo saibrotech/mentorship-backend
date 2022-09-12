2f2b25f529ba8e92dc8478a49b5c011c8782783d
isabelahyeda <isabelahyeda@gmail.com>
2022-09-12 19:47:45 -0300

    job listing

--- job/views.py
+++ job/views.py
@@ -1,7 +1,27 @@
 from django.shortcuts import render
 
 def index(request):
+    areas = [
+        {
+            'name': 'Desenvolvimento'
+        },
+        {
+            'name': 'Suporte'
+        },
+        {
+            'name': 'Infraestrutura'
+        },
+        {
+            'name': 'Ciência de Dados'
+        }, 
+        {
+            'name': 'Segurança'
+        }, 
+        {
+            'name': 'Produto'
+        }
+    ]
     context = {
-        
+        'areas': areas
     }
     return render(request, 'job/index.html', context)
