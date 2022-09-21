22e50ea3690c771aa44b1431e672d8530595ff17
isabelahyeda <isabelahyeda@gmail.com>
2022-09-21 08:59:00 -0300

    filtro busca

--- job/views.py
+++ job/views.py
@@ -1,12 +1,17 @@
 from job.models import Category, Job
 from django.shortcuts import render
+from django_filters.rest_framework import DjangoFilterBackend
 
 def index(request):
     areas = Category.objects.all()
     jobs = Job.objects.all()
+    filter= [DjangoFilterBackend]
+    filterset_fields = ['job']
 
     context = {
         'areas': areas,
         'jobs': jobs,
+        'filter':filter,
+        'filterset_fields':filterset_fields,
     }
     return render(request, 'job/index.html', context)
