2ced94e4a2732220d40b872f9fcdd0dcb85049d3
Frederico Minuzzi <freddyminu@gmail.com>
2022-10-07 12:57:01 -0300

    removed 2 out of 3 filters and fixed the 3rd one

--- job/admin.py
+++ job/admin.py
@@ -9,17 +9,15 @@ from .models import Job
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('id','name', 'main_stacks', 'main_skills', 'pay_range')
     search_fields = ['id','name','main_stacks', 'main_skills']
-    list_filter = ('name','pay_range')
 
 class CompanyAdmin(admin.ModelAdmin):
     list_display = ('id','name', 'overview', 'website', 'industry', 'size', 'headquarters', 'founded', 'specialties')
     search_fields = ['id', 'name', 'industry', 'headquarters', 'founded', 'specialties']
-    list_filter = ('name','size')
 
 class JobAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'type', 'location', 'requirements', 'link', 'category_id', 'company_id')
     search_fields = ['id', 'title', 'type', 'location', 'requirements']
-    list_filter = ('type','location')
+    list_filter = ('id','category')
 
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Company, CompanyAdmin)
