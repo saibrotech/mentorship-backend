b22a2d422446014643a10912ffc5d007ecb0aa21
Frederico Minuzzi <freddyminu@gmail.com>
2022-10-07 09:44:50 -0300

    Added filters for the 3 objects in the database

--- job/admin.py
+++ job/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django import forms
 
 # Register your models here.
 from .models import Category
@@ -9,14 +10,17 @@ from .models import Job
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('id','name', 'main_stacks', 'main_skills', 'pay_range')
     search_fields = ['id','name','main_stacks', 'main_skills']
+    list_filter = ('name','pay_range')
 
 class CompanyAdmin(admin.ModelAdmin):
     list_display = ('id','name', 'overview', 'website', 'industry', 'size', 'headquarters', 'founded', 'specialties')
     search_fields = ['id', 'name', 'industry', 'headquarters', 'founded', 'specialties']
+    list_filter = ('name','size')
 
 class JobAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'type', 'location', 'requirements', 'link', 'category_id', 'company_id')
     search_fields = ['id', 'title', 'type', 'location', 'requirements']
+    list_filter = ('type','location')
 
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Company, CompanyAdmin)
