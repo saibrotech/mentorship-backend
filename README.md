d0aa57644f0d02df6af84c4db727ab785d113a61
Frederico Minuzzi <freddyminu@gmail.com>
2022-10-11 12:37:05 -0300

    added requirements for linter and autoformatter and updated readme file

--- README.md
+++ README.md
@@ -12,6 +12,7 @@ No terminal:
 
 Isso irá rodar o docker apenas da base da dados PostgreSQL
 
+
 ### Create environment
 
 `python3 -m venv venv`
@@ -19,6 +20,7 @@ Isso irá rodar o docker apenas da base da dados PostgreSQL
 Select the interpreter before:
 
 `pip install -r requirements.txt`
+
 
 ### Run server
 
@@ -32,18 +34,22 @@ Select the interpreter before:
 
 `python manage.py runserver`
 
+Access: http://127.0.0.1:8000/job
+
 
 ### Run fixtures
+
 `python manage.py loaddata category`
 
 `python manage.py loaddata company`
 
 `python manage.py loaddata job`
 
+
 ### Admin
 
-http://127.0.0.1:8000/admin
-
+Access: http://127.0.0.1:8000/admin
+    
 After create or edit a model:
 
 `python manage.py makemigrations` to create migration files
@@ -51,9 +57,25 @@ After create or edit a model:
 `python manage.py migrate` to apply to data base
 
 
+### Run Pylinter
+
+`flake8 --format=wemake your_module.py`
+
+> Example:  `flake8 --format=wemake job/` This line of code will run the pylinter on all the python files inside the job folder and it's children.
+
+### Run Auto-Formatter
+
+Tries to automatically fix some of the code "errors" that would show up on the pylinter.
+
+`autopep8 --in-place --aggressive --aggressive <filename>`
+
+> Example:  `autopep8 --in-place --aggressive --aggressive job/`  This line of code will run the auto-formatter on all the python files inside the job folder and it's children.
+
 ### Figma Prototype
 
 https://www.figma.com/file/Qg08r5xH2nlWXmAao32k0I/Vagas-para-J%C3%BAnior?node-id=105%3A466
+
+
 
 ## Produção
 
--- job/models.py
+++ job/models.py
@@ -13,20 +13,23 @@ class Category(models.Model):
     main_stacks = models.CharField(max_length=200)
     main_skills = models.CharField(max_length=200)
     pay_range = models.CharField(max_length=10)
-   
+
     def __str__(self):
         return f'{self.name} ({self.id})'
+
     class Meta:
         verbose_name_plural = 'Categories'
 
+
 class Company(models.Model):
     COMPANY_SIZES = [                   # Source: SEBRAE. Anuario do Trabalho na Micro e Pequena Empresa. Sebrae: Brasília. p.17. 2013.
-        ('XS', 'Up to 10 employees'),   # https://www.sebrae.com.br/Sebrae/Portal%20Sebrae/Anexos/Anuario%20do%20Trabalho%20Na%20Micro%20e%20Pequena%20Empresa_2013.pdf
+        # https://www.sebrae.com.br/Sebrae/Portal%20Sebrae/Anexos/Anuario%20do%20Trabalho%20Na%20Micro%20e%20Pequena%20Empresa_2013.pdf
+        ('XS', 'Up to 10 employees'),
         ('S', '10 to 49 employees'),
         ('M', '50 to 99 employees'),
         ('L', 'Over 100 employees')
     ]
-    
+
     name = models.CharField(max_length=50)
     overview = models.TextField()
     website = models.CharField(max_length=200)
@@ -38,8 +41,10 @@ class Company(models.Model):
 
     def __str__(self):
         return f'{self.name} ({self.id})'
+
     class Meta:
         verbose_name_plural = 'Companies'
+
 
 class Job(models.Model):
     XP_LEVELS = [
@@ -55,7 +60,7 @@ class Job(models.Model):
         ('INTERN', 'Internship'),
         ('OTHER', 'Other')
     ]
-    
+
     title = models.CharField(max_length=50)
     date_posted = models.DateField
     experience_level = models.CharField(max_length=6, choices=XP_LEVELS)
