0d20b4e675a75204a67010532f248a043901a116
Andre Piantino <andre@saibro.tech>
2022-10-11 10:06:08 -0300

    Change API to public

--- api/views.py
+++ api/views.py
@@ -1,7 +1,9 @@
-from rest_framework import viewsets
+from rest_framework import mixins, viewsets
 from api.serializers import JobSerializer
 from job.models import Job
 
-class JobViewSet(viewsets.ModelViewSet):
+class PublicJobViewSet(mixins.RetrieveModelMixin,
+                    mixins.ListModelMixin,
+                    viewsets.GenericViewSet):
     queryset = Job.objects.all()
     serializer_class = JobSerializer
