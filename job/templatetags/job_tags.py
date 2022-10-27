ac49cb1e29756c3829ecf44097d2b0af445d6eb8
Frederico Minuzzi <33817929+Freddyminu@users.noreply.github.com>
2022-10-27 15:06:04 -0300

    added exception to stop the application from crashing (#67)
    * added exception to stop the aplication from crashing if a category doesnt exist in the icon dictionary
    
    * forgot to add CACHE to gitignore
    
    * Delete theme.f43924802cac.css
    
    * removed try except, added if else
    
    * Removed unecessary code
    
    Co-authored-by: Frederico Minuzzi <fred@Fredericos-MacBook-Pro.local>
    Co-authored-by: Andre Piantino <andre@saibro.tech>

--- job/templatetags/job_tags.py
+++ job/templatetags/job_tags.py
@@ -1,4 +1,5 @@
 # custom tags stay in here
+from re import I
 from django import template
 from django.utils.safestring import mark_safe
 
@@ -15,13 +16,18 @@ ICON_DICT = {
 
 # Function that returns a icon for the area depending on the area_code.
 
-
 @register.filter(is_safe=True)
 def area_icon(area_code, chosen_area):
     if area_code == chosen_area:
         background = 'bg-primary'
     else:
         background = 'bg-secondary'
-    class_icon = ICON_DICT[area_code] if ICON_DICT[area_code] else 'bi-bug'
+
+    if area_code in ICON_DICT:
+        class_icon = ICON_DICT[area_code]
+    else:
+        print("The category " + area_code + " doesn't exist.");
+        class_icon = "bi-bug"
+
     safe_text = f'<i class="bi {class_icon} icon {background} text-white rounded-circle"></i>'
     return mark_safe(safe_text)
