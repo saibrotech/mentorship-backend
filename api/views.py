e7f48db2f2aa2b5684235b5725ed47d8b05c1a53
Andre Piantino <andre@saibro.tech>
2022-12-07 10:45:40 -0300

    9 permitir carregar mais vagas (#85)
    * Called Job API on click in button
    
    * Create Job itens from Javascript
    
    * newsletter form
    
    * form newsletter changes
    
    * attribute value changes
    
    * Checked area from URL and added required fields
    
    * Created new-jobs-criteria
    
    * Changed header link button
    
    * Adjusted views text
    
    * Load more jobs from JS
    
    * Showed area icon in js
    
    * Fixed missing code documentation
    
    Co-authored-by: isabelahyeda <isabelahyeda@gmail.com>
    Co-authored-by: Ivan Carlos <ivancarlosduartejunior@gmail.com>

--- api/views.py
+++ api/views.py
@@ -1,13 +1,40 @@
 """Views for api App."""
 
-from rest_framework import viewsets
+from rest_framework import mixins
+from rest_framework.viewsets import GenericViewSet
 
 from api.serializers import JobSerializer
 from job.models import Job
 
+PARAM_AREA = 'area'
+PARAM_SEARCH = 'search'
 
-class PublicJobViewSet(viewsets.ModelViewSet):
+
+class PublicJobViewSet(
+    mixins.RetrieveModelMixin,
+    mixins.ListModelMixin,
+    GenericViewSet,
+):
     """Public view for Job (without autentication)."""
 
-    queryset = Job.objects.all()
     serializer_class = JobSerializer
+
+    def get_queryset(self):
+        """
+        Return the jobs according to search term or by area.
+
+        Returns:
+            A job list
+        """
+        request = self.request
+
+        if PARAM_SEARCH in request.GET:
+            search = request.GET.get(PARAM_SEARCH)
+            jobs = Job.objects.filter(title__icontains=search)
+        elif PARAM_AREA in request.GET:
+            area = request.GET.get(PARAM_AREA)
+            jobs = Job.objects.filter(category__code=area)
+        else:
+            jobs = Job.objects.all()
+
+        return jobs
